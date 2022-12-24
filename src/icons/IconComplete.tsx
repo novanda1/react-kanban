fae08fbb13c2277a35314270b14fe9526922cb06
Novanda Ahsan <novandaahsan1@gmail.com>
2022-12-24 10:29:31 +0700

    feat(icons): fix clip-path, must be clipPath

--- src/icons/IconComplete.tsx
+++ src/icons/IconComplete.tsx
@@ -9,7 +9,7 @@ const IconComplete: FC<React.SVGProps<SVGSVGElement>> = (props) => (
     xmlns="http://www.w3.org/2000/svg"
     {...props}
   >
-    <g clip-path="url(#clip0_4_931)">
+    <g clipPath="url(#clip0_4_931)">
       <path
         d="M8 16C12.4183 16 16 12.4183 16 8C16 3.58172 12.4183 0 8 0C3.58172 0 0 3.58172 0 8C0 12.4183 3.58172 16 8 16Z"
         fill="#43936C"
